003fecb89a2d67b0adf5eedae4fb8fa03acc2b5b
webninjataylor <webninjataylor@gmail.com>
2015-04-08 21:30:53 -0400

    Class 3 - Lab 3 - Creating a new book

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get "/books" => "books#index", as: 'books'
+  get "/books/new" => "books#new", as: 'new_book'
   get "/books/:id" => "books#show", as: 'book'
+  post "/books" => "books#create"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
