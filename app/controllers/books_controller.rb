02cde8c5f6331f5b4db54c5eec41586f1cc067e9
webninjataylor <webninjataylor@gmail.com>
2015-04-15 20:28:14 -0400

    Class 4 - Lab 2 - Showing Validation Errors

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,16 +16,22 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to @book
+    if @book.save
+      redirect_to @book
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
-    @book.update(book_params)
-    redirect_to @book
+    if @book.update(book_params)
+      redirect_to @book
+    else
+      render :new
+    end
   end
 
   def destroy
